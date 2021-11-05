a2da1d23a476dfc33544bf07fe0741e7c27ec2d3
Adam La Fleur <13008923+Oxeus@users.noreply.github.com>
2021-11-04 17:05:28 -0700

    More breaking

--- test/robotbeta/EnlightenmentCenterTest.java
+++ test/robotbeta/EnlightenmentCenterTest.java
@@ -14,7 +14,7 @@ public class EnlightenmentCenterTest {
 
     @Test
     public void sendLocationTest() throws GameActionException {
-        //when(mockRC.canSetFlag(12900)).thenReturn(true);
+        when(mockRC.canSetFlag(12900)).thenReturn(true);
         MapLocation flag = new MapLocation(100, 100);
         Robot.rc = mockRC;
         Robot.sendLocation(flag);
